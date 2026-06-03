f68f01e512f57e90297a0657d713433bd8108be1
rollersteaam <rollersteaam@gmail.com>
2026-06-03 19:36:12 +0100

    feat: brand-conformant google sign in

--- src/components/auth/google-sign-in.tsx
+++ src/components/auth/google-sign-in.tsx
@@ -1,9 +1,15 @@
-import { StyleSheet } from 'react-native';
+import { useEffect } from 'react';
+import { Platform, Pressable, StyleSheet } from 'react-native';
+
+import { Image } from "expo-image";
 
 import { initializeAuth, signIn } from "@/services/auth/GoogleSignIn";
-import { Button, Host, Text } from '@expo/ui';
-import { useEffect } from 'react';
 
+const LoginImage = Platform.select({
+    android: require("@/assets/images/google/Android/svg/dark/android_dark_rd_SI.svg"),
+    ios: require("@/assets/images/google/iOS/svg/dark/ios_dark_rd_SI.svg"),
+    web: require("@/assets/images/google/Web (mobile + desktop)/svg/dark/web_dark_rd_SI.svg")
+}) 
 
 export default function GoogleSignIn() {
     useEffect(() => {
@@ -11,18 +17,24 @@ export default function GoogleSignIn() {
     }, [])
 
     return (
-        <Host matchContents>
-            <Button onPress={signIn}
-            style={{
-                padding: 24
-            }}>
-                <Text textStyle={{
-                    fontSize: 24
-                }}>
-                    Sign in with Google
-                </Text>
-            </Button> 
-        </Host>
+        <Pressable onPress={signIn}>
+            {({ pressed }) => (
+                <Image
+                    source={LoginImage}
+                    contentFit="contain"
+                    style={{
+                        width: 250,
+                        height: 100,
+                        opacity: pressed ? 0.5 : 1,
+                        transform: [
+                            {
+                                scale: pressed ? 0.95 : 1
+                            },
+                        ]
+                    }}
+                />
+            )}
+        </Pressable>
     )
 }
 
